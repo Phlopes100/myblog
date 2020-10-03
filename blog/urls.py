1530fe4f9d828c7b3fde641feb0f4be0a82f6db8
Phlopes <pedrohlc1231@gmail.com>
2020-10-03 18:37:42 -0300

    adicinando arquivos estaticos e forms

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_new, post_detail
+
 
 urlpatterns = [
     path('', post_list, name="post_list"),
+    path('post/<int:id>/', post_detail, name='post_detail'),
+    path('post/new', post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import PostForm
+from django.utils import timezone
 # Create your views here.
+
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, pk=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form':form})
